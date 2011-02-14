e210f49ba643d5ea49e2269df354ecfac54851b9
ddean <ddean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-14 20:43:20 +0000

    Tagging 1.0.5
    git-svn-id: http://plugins.svn.wordpress.org/bp-group-hierarchy/tags/1.0.5@345495 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes.php
@@ -0,0 +1,328 @@
+<?php
+
+/**
+ * Hierarchy-aware extension for Groups class
+ */
+class BP_Groups_Hierarchy extends BP_Groups_Group {
+
+	var $vars = null;
+	
+	function bp_groups_hierarchy( $id, $parent_id = 0 ) {
+		
+		global $bp, $wpdb;
+
+		if(!isset($bp->table_prefix)) {
+			bp_core_setup_globals();
+		}
+		if(!isset($bp->groups)) {
+			groups_setup_globals();
+		}
+		
+		if(!is_numeric($id)) {
+			$id = $this->check_slug( $id, $parent_id );
+		}
+		
+		if ( $id ) {
+			$this->id = $id;
+			$this->populate();
+		}
+	}
+	
+	function populate() {
+		global $wpdb, $bp;
+
+		parent::populate();
+		if ( $group = $wpdb->get_row( $wpdb->prepare( "SELECT g.* FROM {$bp->groups->table_name} g WHERE g.id = %d", $this->id ) ) ) {
+			$this->parent_id = $group->parent_id;
+			$this->true_slug = $this->slug;
+			$this->path = $this->buildPath();
+			$this->slug = $this->path;
+		}
+	}
+	
+	function buildPath() {
+		
+		$path = $this->true_slug;
+		if($this->parent_id == 0) {
+			return $path;
+		}
+		
+		$parent = (object)array('parent_id'=>$this->parent_id);
+		do {
+			$parent = new BP_Groups_Hierarchy($parent->parent_id);
+			$path = $parent->true_slug . '/' . $path;
+		}
+		while($parent->parent_id != 0);
+		
+		return $path;
+	}
+	
+	function save() {
+		
+		global $bp, $wpdb;
+		
+		$this->slug = $this->true_slug;
+		parent::save();
+		
+		if($this->id) {
+			$sql = $wpdb->prepare(
+				"UPDATE {$bp->groups->table_name} SET
+					parent_id = %d
+				WHERE
+					id = %d
+				",
+				$this->parent_id,
+				$this->id
+			);
+			
+			if ( false === $wpdb->query($sql) ) {
+				return false;
+			}
+
+			if ( !$this->id ) {
+				$this->id = $wpdb->insert_id;
+			}
+			
+			$this->path = $this->buildPath();
+			$this->slug = $this->path;
+			
+			return true;
+		}
+		return false;
+	}
+	
+	/**
+	 * Does the passed group or current group have children?
+	 * @param int GroupID optional ID of a group to check for children (will use current group object if omitted)
+	 */
+	function has_children( $id = null) {
+		global $bp, $wpdb;
+		if(is_null($id)) {
+			if(!isset($this->id) || $this->id == 0)	return false;
+			$id = $this->id;
+		}
+		return $wpdb->get_var($wpdb->prepare("SELECT COUNT(DISTINCT g.id) FROM {$bp->groups->table_name} g WHERE g.parent_id=%d",$id));
+	}
+	
+	/**
+	 * Is the passed group a child of the current object, or the passed group id?
+	 * @param int ChildGroupID ID of suspected child group
+	 * @param int ParentID ID of parent group (optional - will refer to current group object if omitted)
+	 */
+	function is_child( $group_id, $parent_id = null ) {
+		if(is_null($parent_id)) {
+			if(!isset($this->id) || $this->id == 0)	return false;
+			$parent_id = $this->id;
+		}
+		return $wpdb->get_var($wpdb->prepare("SELECT COUNT(g.id) FROM {$bp->groups->table_name} g WHERE g.parent_id=%d AND g.id = %d",$parent_id, $group_id));
+	}
+	
+	function check_slug( $slug, $parent_id ) {
+		global $wpdb, $bp;
+
+		if ( !$slug )
+			return false;
+
+		return $wpdb->get_var( $wpdb->prepare( "SELECT id FROM {$bp->groups->table_name} WHERE slug = %s AND parent_id = %d", $slug, $parent_id ) );
+		
+	}
+	
+	function check_slug_stem( $path ) {
+		
+		global $bp, $wpdb;
+		
+		if(strpos( $path, '/' )) {
+			$path = explode('/',$path);
+			$path = $path[count($path)-1];
+		}
+		if(strlen($path) == 0)	return array();
+		
+		$slug = esc_sql(like_escape(stripslashes($path)));
+		return $wpdb->get_col( "SELECT slug FROM {$bp->groups->table_name} WHERE slug LIKE '$slug%'" );
+		
+	}
+	
+	function group_exists( $path, $parent_id = 0 ) {
+		
+		if(strpos( $path, '/' )) {
+			$path = explode('/',$path);
+			$parent = $parent_id;
+			foreach($path as $slug) {
+				if($parent = self::check_slug( $slug, $parent )) {
+					// Nothing to see here - keep descending into the path
+				} else {
+					return false;
+				}
+			}
+			return $parent;
+		} else {
+			return self::check_slug( $path, $parent_id );
+		}
+		
+	}
+	
+	function get_id_from_slug( $slug, $parent_id = 0 ) {
+		return self::group_exists( $slug, $parent_id );
+	}
+	
+	/**
+	 * Get the full path for a group
+	 */
+	function get_path( $group_id ) {
+		$group = new BP_Groups_Hierarchy( $group_id );
+		if($group) {
+			return $group->path;
+		}
+		return false;
+	}
+	
+	function get_by_parent( $parent_id, $limit = null, $page = null, $user_id = false, $search_terms = false, $populate_extras = true ) {
+		global $wpdb, $bp;
+
+		if ( !is_super_admin() )
+			$hidden_sql = $wpdb->prepare( " AND status != 'hidden'");
+
+		if ( $limit && $page ) {
+			$pag_sql = $wpdb->prepare( " LIMIT %d, %d", intval( ( $page - 1 ) * $limit), intval( $limit ) );
+			$total_groups = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT(DISTINCT g.id) FROM {$bp->groups->table_name_groupmeta} gm1, {$bp->groups->table_name_groupmeta} gm2, {$bp->groups->table_name} g WHERE g.id = gm1.group_id AND g.id = gm2.group_id AND gm2.meta_key = 'last_activity' AND gm1.meta_key = 'total_member_count' AND g.parent_id = $parent_id {$hidden_sql} {$search_sql}" ) );
+		}
+
+		$paged_groups = $wpdb->get_results( $wpdb->prepare( "SELECT g.*, gm1.meta_value as total_member_count, gm2.meta_value as last_activity FROM {$bp->groups->table_name_groupmeta} gm1, {$bp->groups->table_name_groupmeta} gm2, {$bp->groups->table_name} g WHERE g.id = gm1.group_id AND g.id = gm2.group_id AND gm2.meta_key = 'last_activity' AND gm1.meta_key = 'total_member_count' AND g.parent_id = $parent_id {$hidden_sql} {$search_sql} ORDER BY g.name ASC {$pag_sql}"  ) );
+
+		foreach ( (array)$paged_groups as $key => $group ) {
+			$paged_groups[$key] = new BP_Groups_Hierarchy( $group->id );
+		}
+
+		if ( !empty( $populate_extras ) ) {
+			foreach ( (array)$paged_groups as $group ) $group_ids[] = $group->id;
+			$group_ids = $wpdb->escape( join( ',', (array)$group_ids ) );
+			$paged_groups = self::get_group_extras( &$paged_groups, $group_ids, 'newest' );
+		}
+
+		return array( 'groups' => $paged_groups, 'total' => $total_groups );
+	}
+
+	function get_group_extras( $paged_groups, $group_ids, $type = false ) {
+
+		foreach($paged_groups as $key => $group) {
+			if(!isset($group->path)) {
+				$paged_groups[$key]->path = self::get_path($group->id);
+				$paged_groups[$key]->slug = $paged_groups[$key]->path;
+			}
+		}
+
+		if ( empty( $group_ids ) )
+			return $paged_groups;
+
+		return parent::get_group_extras( $paged_groups, $group_ids, $type );
+		
+	}
+	
+	
+	function __isset($varName) {
+		return array_key_exists($varName,$this->vars);
+	}
+	
+	function __set($varName, $value) {
+		$this->vars[$varName] = $value;
+	}
+	
+	function __get($varName) {
+		return $this->vars[$varName];
+	}
+	
+	
+}
+
+/**
+ * Hierarchy-aware extension for Groups template class
+ */
+class BP_Groups_Hierarchy_Template extends BP_Groups_Template {
+
+	var $vars = array();
+
+	function bp_groups_hierarchy_template( ) {
+		$args = func_get_args();
+		if(is_array($args) && count($args) > 1) {
+			list(
+				$params['user_id'],
+				$params['type'],
+				$params['page'],
+				$params['per_page'],
+				$params['max'],
+				$params['slug'],
+				$params['search_terms'],
+				$params['populate_extras'],
+				$params['parent_id']
+			) = $args;
+			$this->params = $params;
+			call_user_func_array(array(parent,'bp_groups_template'),$args);
+			$this->synchronize();
+		} else {
+			$this->params = array();
+		}
+	}
+
+	/**
+	 * Since we don't always have access to the params passed to BP_Groups_Template
+	 * we have to wait until after constructor has run to fill in details
+	 */
+	function synchronize() {
+		global $bp;
+				
+		if(isset($this->params) && array_key_exists('parent_id',$this->params)) {
+	
+			/**
+			 * Fill in requests by parent_id for tree traversal on admin side
+			 */
+			$this->groups = bp_group_hierarchy_get_by_hierarchy($this->params);
+			$this->groups = $this->groups['groups'];
+			$this->group_count = count($this->groups);
+			$this->total_group_count = count($this->groups);
+			
+		} else if($this->single_group && $bp->groups->current_group) {
+			/**
+			 * Groups with multi-level slugs are missed by the parent.
+			 * Fill them in from $bp->groups->current_group
+			 */
+			$this->groups = array(
+				(object)array(
+					'group_id'	=> $bp->groups->current_group->id
+				)
+			);
+			$this->group_count = 1;
+		}
+		
+	}
+
+	function the_group() {
+		global $group;
+
+		$this->in_the_loop = true;
+		$this->group = $this->next_group();
+
+		if ( $this->single_group )
+			$this->group = new BP_Groups_Hierarchy( $this->group->group_id );
+		else {
+			if ( $this->group )
+				wp_cache_set( 'groups_group_nouserdata_' . $group->group_id, $this->group, 'bp' );
+		}
+
+		if ( 0 == $this->current_group ) // loop has just started
+			do_action('loop_start');
+	}
+
+	function __isset($varName) {
+		return array_key_exists($varName,$this->vars);
+	}
+	
+	function __set($varName, $value) {
+		$this->vars[$varName] = $value;
+	}
+	
+	function __get($varName) {
+		return $this->vars[$varName];
+	}
+
+}
+?>
---
+++ functions.php
@@ -0,0 +1,250 @@
+<?php
+
+/**
+ *	Override group retrieval for groups template
+ *  @return int|bool number of matching groups or FALSE if none
+ */
+function bp_group_hierarchy_override_template($has_groups) {
+	
+	global $bp, $groups_template;
+
+	if(!$has_groups)	return false;
+	
+	$groups_hierarchy_template = new BP_Groups_Hierarchy_Template();
+
+	bp_group_hierarchy_copy_vars(
+		$groups_template,
+		$groups_hierarchy_template, 
+		array(
+			'group',
+			'group_count',
+			'groups',
+			'single_group',
+			'total_group_count',
+			'pag_links',
+			'pag_num',
+			'pag_page'
+		)
+	);
+
+	$groups_hierarchy_template->synchronize();
+
+	foreach($groups_hierarchy_template->groups as $key => $group) {
+		if(isset($group->id)) {
+			$groups_hierarchy_template->groups[$key] = new BP_Groups_Hierarchy($group->id);
+		} else {
+//			$groups_hierarchy_template->groups[$key] = new BP_Groups_Hierarchy($group->group_id);	
+		}
+	}
+	$groups_template = $groups_hierarchy_template;
+	
+	return $has_groups;
+}
+add_filter( 'bp_has_groups', 'bp_group_hierarchy_override_template', 10, 2 );
+
+
+/**
+ * Fix forum topic permalinks for subgroups
+ */
+function bp_group_hierarchy_fixup_forum_paths( $topics ) {
+	
+	// replace each simple slug with its full path
+	if(is_array($topics)) {
+		foreach($topics as $key => $topic) {
+	
+			$group_id = BP_Groups_Group::group_exists($topic->object_slug);
+			if($group_id) {
+				$topics[$key]->object_slug = BP_Groups_Hierarchy::get_path( $group_id );
+			}
+		}
+	}
+	return $topics;
+	
+}
+add_filter( 'bp_forums_get_forum_topics', 'bp_group_hierarchy_fixup_forum_paths', 10, 2 );
+
+/**
+ * Fix forum topic action links (Edit, Delete, Close, Sticky, etc.)
+ */
+function bp_group_hierarchy_fixup_forum_links( $has_topics ) {
+	global $forum_template;
+	
+	$group_id = BP_Groups_Group::group_exists( $forum_template->topic->object_slug );
+	$forum_template->topic->object_slug = BP_Groups_Hierarchy::get_path( $group_id );
+	
+	return $has_topics;
+	
+}
+add_filter( 'bp_has_topic_posts', 'bp_group_hierarchy_fixup_forum_links', 10, 2 );
+
+/************************************
+ * Utility and replacement functions
+ ***********************************/
+
+function bp_group_hierarchy_copy_vars($from, &$to, $attribs) {
+	foreach($attribs as $var) {
+		if(isset($from->$var)) {
+			$to->$var = $from->$var;
+		}
+	}
+}
+
+/**
+ * Hierarchy-aware replacement for bp_has_groups
+ */
+function bp_has_groups_hierarchy($args = '') {
+	global $groups_template, $bp;
+
+	/***
+	 * Set the defaults based on the current page. Any of these will be overridden
+	 * if arguments are directly passed into the loop. Custom plugins should always
+	 * pass their parameters directly to the loop.
+	 */
+	$type = 'active';
+	$user_id = false;
+	$search_terms = false;
+	$slug = false;
+
+	/* User filtering */
+	if ( !empty( $bp->displayed_user->id ) )
+		$user_id = $bp->displayed_user->id;
+
+	/* Type */
+	if ( 'my-groups' == $bp->current_action ) {
+		if ( 'most-popular' == $order )
+			$type = 'popular';
+		else if ( 'alphabetically' == $order )
+			$type = 'alphabetical';
+	} else if ( 'invites' == $bp->current_action ) {
+		$type = 'invites';
+	} else if ( $bp->groups->current_group->slug ) {
+		$type = 'single-group';
+		$slug = $bp->groups->current_group->slug;
+	}
+
+	if ( isset( $_REQUEST['group-filter-box'] ) || isset( $_REQUEST['s'] ) )
+		$search_terms = ( isset( $_REQUEST['group-filter-box'] ) ) ? $_REQUEST['group-filter-box'] : $_REQUEST['s'];
+
+	$defaults = array(
+		'type' => $type,
+		'page' => 1,
+		'per_page' => 20,
+		'max' => false,
+
+		'user_id' => $user_id, // Pass a user ID to limit to groups this user has joined
+		'slug' => $slug, // Pass a group slug to only return that group
+		'search_terms' => $search_terms, // Pass search terms to return only matching groups
+
+		'populate_extras' => true // Get extra meta - is_member, is_banned
+	);
+
+	$r = wp_parse_args( $args, $defaults );
+
+	extract( $r );
+
+	if(isset($parent_id)) {
+		$type = 'by_parent';
+	}
+
+	$groups_template = new BP_Groups_Hierarchy_Template( (int)$user_id, $type, (int)$page, (int)$per_page, (int)$max, $slug, $search_terms, (bool)$populate_extras, (int)$parent_id );
+
+	return apply_filters( 'bp_has_groups', $groups_template->has_groups(), &$groups_template );
+
+}
+
+/**
+ * Catch requests for groups by parent and use BP_Groups_Hierarchy::get_by_parent to handle
+ */
+function bp_group_hierarchy_get_by_hierarchy($args) {
+
+	$defaults = array(
+		'type' => 'active', // active, newest, alphabetical, random, popular, most-forum-topics or most-forum-posts
+		'user_id' => false, // Pass a user_id to limit to only groups that this user is a member of
+		'search_terms' => false, // Limit to groups that match these search terms
+
+		'per_page' => 20, // The number of results to return per page
+		'page' => 1, // The page to return if limiting per page
+		'populate_extras' => true, // Fetch meta such as is_banned and is_member
+	);
+
+	$params = wp_parse_args( $args, $defaults );
+	
+	extract( $params, EXTR_SKIP );
+
+	switch ( $type ) {
+		case 'by_parent':
+			$groups = BP_Groups_Hierarchy::get_by_parent( $parent_id, $per_page, $page, $user_id, $search_terms, $populate_extras );
+			break;
+	}
+	
+	return $groups;
+}
+
+
+/**
+ * Override the group slug in permalinks with a group's full path
+ * NOTE: this may obviate some of the other functions; they will disappear over time if that turns out to be the case
+ */
+function bp_group_hierarchy_fixup_permalink( $permalink ) {
+	
+	global $bp;
+	
+	$group_slug = substr( $permalink, strlen( $bp->root_domain . '/' . $bp->groups->slug . '/' ), -1 );
+	
+	if(strpos($group_slug,'/'))	return $permalink;
+	
+	$group_id = BP_Groups_Group::get_id_from_slug( $group_slug );
+	
+	if( !is_null($group_id) ) {
+		$group_path = BP_Groups_Hierarchy::get_path( $group_id );
+		return str_replace($group_slug,$group_path,$permalink);
+	}
+	return $permalink;
+	
+}
+add_filter( 'bp_get_group_permalink', 'bp_group_hierarchy_fixup_permalink' );
+
+/**
+ * Group-specific copy of avatar retrieval function - used for the group hierarchy extension
+ */
+function bp_group_hierarchy_get_avatar_by_group( $args = '', $group = false ) {
+	global $bp, $groups_template;
+	
+	$defaults = array(
+		'type' => 'full',
+		'width' => false,
+		'height' => false,
+		'class' => 'avatar',
+		'id' => false,
+		'alt' => __( 'Group avatar', 'buddypress' )
+	);
+	
+	$r = wp_parse_args( $args, $defaults );
+	extract( $r, EXTR_SKIP );
+	
+	if ( !$group )
+		$group =& $groups_template->group;
+	
+	
+	/* Fetch the avatar from the folder, if not provide backwards compat. */
+	if ( !$avatar = bp_core_fetch_avatar( array( 'item_id' => $group->id, 'object' => 'group', 'type' => $type, 'avatar_dir' => 'group-avatars', 'alt' => $alt, 'css_id' => $id, 'class' => $class, 'width' => $width, 'height' => $height ) ) )
+		$avatar = '<img src="' . esc_attr( $group->avatar_thumb ) . '" class="avatar" alt="' . esc_attr( $group->name ) . '" />';
+	
+	return apply_filters( 'bp_get_group_avatar', $avatar );
+}
+
+/**
+ * Group-specific copy of member count retrieval function - used for the group hierarchy extension
+ */
+function bp_group_hierarchy_get_group_member_count_by_group( $group = false ) {
+	global $groups_template;
+
+	if ( !$group )
+		$group =& $groups_template->group;	
+
+	if ( 1 == (int) $group->total_member_count )
+		return apply_filters( 'bp_get_group_member_count', sprintf( __( '%s member', 'buddypress' ), bp_core_number_format( $group->total_member_count ) ) );
+	else
+		return apply_filters( 'bp_get_group_member_count', sprintf( __( '%s members', 'buddypress' ), bp_core_number_format( $group->total_member_count ) ) );
+}
+?>
