fbf2aeb08f2e53cd1d168a06e079fa2a8b0910e5
ddean <ddean@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-17 19:35:01 +0000

    Tagging 1.1.1
    git-svn-id: http://plugins.svn.wordpress.org/bp-group-hierarchy/tags/1.1.1@398532 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/hierarchy.js
@@ -0,0 +1,43 @@
+jQuery(document).ready( function() {
+	/** Add tree class to groups panel for AJAX loading */
+	jQuery('div.groups').addClass('tree');
+	jQuery('.item-subitem-indicator a').live('click',function(event) {
+
+		if( jq(this).html() == '[-]' && jq(this).parents('li').has('div.subitem').length ) {
+			jq(this).parent().parent().children('div.subitem').remove();
+			jq(this).html('[+]');
+			return false;
+		}
+		
+		if(jq(this).html() != '[+]')
+			return false;
+		
+		/** From BuddyPress global.js - modified for subitem loading */
+		if ( jQuery(this).hasClass('no-ajax') )
+			return;
+
+		var target = jQuery(event.target).parents('li');
+		target = target[0];
+		
+		if ( jq(target).is('li') ) {
+			var css_id = jq(target).attr('id').split( '-' );
+			
+			var object = css_id[0];
+
+			if ( 'activity' == object )
+				return false;
+
+			var scope = css_id[1];
+			var filter = jq("#" + object + "-order-select select").val();
+			var search_terms = jq("#" + object + "_search").val();
+			
+			target = jq('<div />').appendTo(target);
+			target.addClass('subitem');
+
+			bp_filter_request( object, filter, scope, target, search_terms, 1, jq.cookie('bp-' + object + '-extras') );
+		}
+
+		jq(this).html('[-]');
+		return false;
+	});
+});
